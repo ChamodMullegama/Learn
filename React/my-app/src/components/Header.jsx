86620df86eb7249b3ca08c78a868b67093fadc99
ChamodMullegama <chamod.mullegama@gmail.com>
2025-11-02 14:14:21 +0530

    Initialize React app with Vite and ESLint config
    Set up a new React project using Vite, including initial configuration files, ESLint setup, and basic component structure. Added .gitignore, README, index.html, Vite and ESLint configs, and starter components for a minimal working app.

---
+++ React/my-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>hello my web site</h1>
+        <a href="">Home</a><a href="">About</a><a href="">Contact</a>
+    </div>
+  )
+}
+
+export default Header
