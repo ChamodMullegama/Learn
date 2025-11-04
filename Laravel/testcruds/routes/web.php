b3d75b4ceae9c9b678864128bf060b3c25816fa6
ChamodMullegama <chamod.mullegama@gmail.com>
2025-11-04 23:01:05 +0530

    Add basic layout, navigation, and product route
    Introduced a main layout with Bootstrap integration, a reusable navigation bar component, and updated home and product views to extend the layout. Added a product route and controller method, replacing the default home route with a custom view.

--- Laravel/testcruds/routes/web.php
+++ Laravel/testcruds/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home.index');
+});
+
+Route::prefix('product')->group(function(){
+    Route::get('/',[ProductController::class, 'index'])->name('product.index');
 });
