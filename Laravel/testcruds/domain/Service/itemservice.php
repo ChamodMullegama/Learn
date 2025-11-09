4a5305fb1b500a8fcfc7fbf38bbb3bc2218d00d1
ChamodMullegama <chamod.mullegama@gmail.com>
2025-11-10 03:56:53 +0530

    update crud again

--- Laravel/testcruds/domain/Service/itemservice.php
+++ Laravel/testcruds/domain/Service/itemservice.php
@@ -1,31 +1,32 @@
 <?php
- namespace domain\Service;
+
+namespace domain\Service;
 
 use App\Models\item;
 use Illuminate\Support\Facades\Storage;
 
- class itemservice
- {
-
-protected $item;
+class itemservice
+{
+    protected $item;
 
     public function __construct(item $item)
     {
-       $this->item= $item;
+        $this->item = $item;
     }
 
     public function index()
     {
-        return $this->item->select('id','name','des')->get();
+        return $this->item->select('id', 'name', 'image')->get();
     }
 
     public function store(array $data)
     {
-          if(request()->hasFile('image')){
-             $data['imagE']=request()->file('image')->store('item' , 'public');
-          }
 
-          return $this->item->create($data);
+        if (request()->hasFile('image')) {
+            $data['image'] = request()->file('image')->store('item', 'public/');
+        }
+
+        return $this->item->create($data);
     }
 
     public function edit($id)
@@ -33,32 +34,26 @@ protected $item;
         return $this->item->findOrFail($id);
     }
 
-    public function update($id,array $data)
+    public function update($id, array $data)
     {
-        $item=$this->item->findOrFail($id);
-        if(request()->hasFile('image')){
-            if($item->image && Storage::exists('public/' .$item->image)){
-                Storage::delete('public/' .$item->image);
+        $item = $this->item->findOrFail($id);
+
+        if (request()->hasFile('image')) {
+            if ($item->image && Storage::exists('public/' . $item->image)) {
+                Storage::delete('public/' . $item->image);
+            } else {
+                unset($date['image']);
             }
-
-            $data['image'] = request()->file('image')->store('item' , 'public');
-
-        }else{
-            unset($data['image']);
         }
 
         return $item->update($data);
-
     }
 
-    public function destroy($id)
+    public function delete($id)
     {
-        $item=$this->item->findOrFail($id);
-
-        if($item->image && Storage::exists('public/' . $item->image)){
-           Storage::delete('public/' .$item->image);
+        $item = $this->item->findOrFail($id);
+        if ($item->image && Storage::exists('public/' . $item->image)) {
+            Storage::delete('public/' . $item->image);
         }
-
-        return $item->delete();
     }
- }
+}
