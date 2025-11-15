8f3e62906c3a1df43548b4b4290372ff158148dd
ChamodMullegama <chamod.mullegama@gmail.com>
2025-11-15 16:20:43 +0530

    start the go learn

---
+++ go/bookingApp/go.mod
@@ -0,0 +1,3 @@
+module BOOKINGAPP
+
+go 1.25.4
---
+++ go/bookingApp/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	myAge := "i am 24 years old"
+	const totaltiket =50
+	var avalabeltiket uint = 50
+
+	var bookings [50]string
+
+	fmt.Print("hi chamod kohomda i am " , myAge , "i am good for nice")
+	fmt.Printf("\nwe have %v and still avalbel %v" ,totaltiket, avalabeltiket)
+
+var fname string
+var lname string
+var email string
+var usertiket uint
+	
+fmt.Println("enter your first name:")
+fmt.Scan(&fname)
+
+fmt.Println("enter your last name:")
+fmt.Scan(&lname)
+
+fmt.Println("enter your email name:")
+fmt.Scan(&email)
+
+fmt.Println("enter your need tiket:")
+fmt.Scan(&usertiket)
+
+avalabeltiket = avalabeltiket - usertiket
+bookings[0]=fname +""+lname
+
+fmt.Printf("your name is %v %v and you book %v tiket and youn  comformation email sent to the %v " ,fname,lname,email,usertiket)
+fmt.Printf("now avalabel tiket is : %v", avalabeltiket)
+}
