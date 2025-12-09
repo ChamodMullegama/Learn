b7afaea49a007780deecfbb29ff1ed618d66f49f
ChamodMullegama <chamod.mullegama@gmail.com>
2025-12-09 21:44:57 +0530

    new api rroute

--- Laravel/testcruds/routes/api.php
+++ Laravel/testcruds/routes/api.php
@@ -6,10 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
-// })->middleware('auth:sanctum');
-
-
-
-
+// })->middleware('auth:sanctum')
 
 Route::resource('post',PostController::class);
+Route::resource('food',PostController::class);
+
+
+
+
