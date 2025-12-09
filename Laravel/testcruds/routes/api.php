de8107602fb482cc83082c9a0a2f7ca4b0fbfa4f
ChamodMullegama <chamod.mullegama@gmail.com>
2025-12-09 21:46:21 +0530

    change

--- Laravel/testcruds/routes/api.php
+++ Laravel/testcruds/routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum')
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum')
 
 Route::resource('post',PostController::class);
 
