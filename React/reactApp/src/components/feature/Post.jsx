8472eb282bfd08f9f979a427d9e8d397b7c20f28
ChamodMullegama <chamod.mullegama@gmail.com>
2025-12-24 03:07:59 +0530

    useEffact  hools (updating)

--- React/reactApp/src/components/feature/Post.jsx
+++ React/reactApp/src/components/feature/Post.jsx
@@ -1,47 +1,57 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Post = () => {
+  const [posts, setPosts] = useState([]);
+  const [query, setQuery] = useState("");
 
-const [posts, setPosts] = useState([])
+  useEffect(() => {
+    console.log("Post component mounted");
 
-useEffect(() => {
-    console.log("Post component mounted")
+  fetch(`https://jsonplaceholder.typicode.com/posts?title_like=${query}`)
 
-    fetch("https://jsonplaceholder.typicode.com/posts").then(res => {
-        if(!res.ok) throw new Error("Network response was not ok")
-        return res.json()
-    }).then(data=>setPosts(data))
-}, [])
+      .then((res) => {
+        if (!res.ok) throw new Error("Network response was not ok");
+        return res.json();
+      })
+      .then((data) => setPosts(data));
+  }, [query]);
+
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+  }
 
   return (
     <div>
-        {posts.length >0 ? (
-          <>
-            {posts.map((post) => {
-              return (
-                <table key={post.id}>
-                  <thead>
-                    <tr>
-                      <th>Title</th>
-                      <th>Body</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>{post.title}</td>
-                      <td>{post.body}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              )
-            })}
-          </>
-        ) : (
-            <p>no post</p>
-        )}
-        
+        <input type="text" placeholder="search post"  onChange={handleSearch}/>
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                </tr>
+            </thead>
+      {posts.length > 0 ? (
+        <>
+          {posts.map((post) => {
+            return (
+              <tbody key={post.id}>
+                <tr>
+                  <td>{post.id}</td>
+                  <td>{post.title}</td>
+                  <td>{post.body}</td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </>
+      ) : (
+        <p>no post</p>
+      )}
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
