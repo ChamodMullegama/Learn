5411ab31450efe06d5f618164380a4bda0e56506
ChamodMullegama <chamod.mullegama@gmail.com>
2026-01-07 21:36:17 +0530

    Add service layer for the product

---
+++ Laravel/app/domain/services/ProductService.php
@@ -0,0 +1,21 @@
+<?php
+ namespace domain\services;
+
+use App\Models\Product;
+
+ class ProductService
+ {
+
+    protected $product;
+
+
+    public function __construct(Product $product)
+    {
+
+        $this->product=$product;
+     }
+
+public
+
+ }
+
