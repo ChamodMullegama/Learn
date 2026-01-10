dac644a61893a6093d20ee68cb9f5d409cb80a6b
ChamodMullegama <chamod.mullegama@gmail.com>
2026-01-10 23:18:25 +0530

    update the code

--- Laravel/app/resources/views/layouts/app.blade.php
+++ Laravel/app/resources/views/layouts/app.blade.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @include("lib.css")
+    @include('lib.css')
 </head>
+
 <body>
-@include("components.mavBar")
-@yield("content")
-@include("lib.js")
-
-
-
+    @include('components.mavBar')
+    @yield('content')
+    @include('lib.js')
 </body>
+
 </html>
